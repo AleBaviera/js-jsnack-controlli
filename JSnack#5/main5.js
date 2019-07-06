a05e6b7af4ae6b8e0e3d5e2614d59743c91123b9
alebaviera <baviera.alessandra@gmailcom>
2019-07-06 10:11:15 +0200

    secondo controllo NUMERO!

--- JSnack#5/main5.js
+++ JSnack#5/main5.js
@@ -2,20 +2,28 @@
 // un numero, se è dispari inseriscilo nell’array
 
 
-// creo array
+// dichiaro var
 var array, num;
 
+// creo array
 array = [];
+
 // inserisco 6 volte un numero
 for (var i = 0; i < 6; i++) {
 
   num = parseInt(prompt('inserisci un numero'));
 
   // se è dispari lo pusho nell'array
-  if (num % 2 != 0){
-    array.push(num);
+  if (Number.isNaN(num)){   //vale su input?
+     alert('HO DETTO NUMERO!');
+
+   }
+
+  else if(num % 2 != 0){
+   array.push(num);
+
 
   }
-
+ console.log(num);
 }
 console.log(array);

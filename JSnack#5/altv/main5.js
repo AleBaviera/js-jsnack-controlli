7a8375d813b69b8bb35ce123d682d507932b4d94
alebaviera <baviera.alessandra@gmailcom>
2019-07-06 15:01:04 +0200

    inserisco l'array in una lista

--- JSnack#5/altv/main5.js
+++ JSnack#5/altv/main5.js
@@ -3,10 +3,11 @@
 
 
 // dichiaro var
-var array, num;
+var array, num, elemento, eletemp;
 
 // creo array
 array = [];
+elemento = document.getElementById('mio-id');
 
 // inserisco 6 volte un numero
 for (var i = 0; i < 6; i++) {
@@ -22,8 +23,15 @@ for (var i = 0; i < 6; i++) {
   else if(num % 2 != 0){
    array.push(num);
 
-
   }
  console.log(num);
+
+
 }
 console.log(array);
+
+for (var i = 0; i < array.length; i++) {
+  eletemp = elemento.innerHTML;
+  elemento.innerHTML = eletemp + '<li>' + array[i] + '</li>';
+  console.log('eletemp è ' + eletemp);
+}

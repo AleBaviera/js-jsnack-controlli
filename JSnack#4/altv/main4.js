764494218fa29c6d59558326326d98928906f9fa
alebaviera <baviera.alessandra@gmailcom>
2019-07-06 15:42:43 +0200

    alert

--- JSnack#4/altv/main4.js
+++ JSnack#4/altv/main4.js
@@ -1,9 +1,13 @@
 
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
+// se sei invitato, allora puoi portare un amico che si aggiunge alla lista
+
 
 // dichiaro var e array
-var lista, nome, invito;
+var lista, nome, invito, elemento, amico, nomeamico;
+elemento = document.getElementById('mio-id');
+
 lista = [
   'pinco',
   'pallo',
@@ -27,3 +31,42 @@ for (var i = 0; i < lista.length; i++) {
 
 // rispondo
 console.log(invito);
+
+
+
+
+// porta un amico?
+if (invito === 'sei invitato'){
+  amico = prompt('porti qualcuno? si/no');
+}
+else {
+  elemento.innerHTML = 'non sei stato invitato :('
+}
+// nome amico
+
+if (amico === 'si'){
+  nomeamico = prompt('aggiungi in lista il suo nome');
+}
+else if(amico === 'no'){
+  elemento.innerHTML = 'ti aspettiamo';
+}
+else{
+  alert('non ho capito');
+  amico = prompt('porti qualcuno? si/no');
+}
+// push nell'array
+lista.push(nomeamico);
+
+// stampa nuova lista
+console.log(lista);
+
+
+
+// visualizza in pagina messaggio
+
+if(invito === 'sei invitato' && amico === 'si'){
+  elemento.innerHTML = 'ti aspettiamo con ' + nomeamico;
+}
+else if (invito === 'sei invitato' ){
+  elemento.innerHTML = 'ti aspettiamo';
+}
